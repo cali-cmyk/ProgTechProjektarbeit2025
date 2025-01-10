fa1202ea7b119cd2d0136301d5efeb0ff4e04ada
Veldnastar <tim.engelhart@student.hnu.de>
2025-01-10 22:50:44 +0100

    Klasse Runde mit meinem Teil manuell gemerged
    -Kommentare wurden auch hinzugefügt
    -kann Programme immer noch nicht ausprobieren

--- ProgTechProjektarbeit2025/src/main/java/Projektarbeit2025/Runde.java
+++ ProgTechProjektarbeit2025/src/main/java/Projektarbeit2025/Runde.java
@@ -3,11 +3,22 @@ package Projektarbeit2025;
 import javax.swing.*;
 
 public class Runde extends JFrame {
+
+	//Attribute
+	private boolean doppeltePunkte;
+	private boolean verdreifachePunkte;
+
 	private JLabel RundeLabel;
 	private JPanel RundePanel;
 	private JList testList;
 
-	public Runde(){
+	//Konstruktor
+	public Runde(int punkte, String name) {
+		super(punkte, name);        //erbt aus Klasse Spiel
+
+	}
+
+	public Runde(){			//zählt das auch als Konstruktor?
 		// erstellt fenster
 		setTitle("Runde");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -16,10 +27,34 @@ public class Runde extends JFrame {
 		setVisible(true);
 
 		//testList.get
+		ArrayList<Runde>  punkteListe;
+		punkteListe = new ArrayList<>();
 
 	}
 
+	public void verdoppelPunkte()
+	{
+		if(doppeltePunkte)
+		{
+			punkte = punkte * 2;
+		}
+	}
+
+	public void verdreifachePunkte()
+	{
+		if(verdreifachePunkte)
+		{
+			punkte = punkte * 3;
+		}
+	}
+
+	public void verrringereGewinnPunkte()
+	{
+		//Hier werden die Punkte von den GewinnPunkten des jeweiligen Spielers subtrahiert bis sie genau 0 erreichen
+	}
+
 	public static void main(String[] args) {
+
 		new Runde();
 	}
 }
