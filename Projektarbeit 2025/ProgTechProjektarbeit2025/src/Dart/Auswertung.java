e680c51c25188d70d5ea912f615053e03c82e66b
Veldnastar <tim.engelhart@student.hnu.de>
2025-01-18 23:50:18 +0100

    test4

---
+++ Projektarbeit 2025/ProgTechProjektarbeit2025/src/Dart/Auswertung.java
@@ -0,0 +1,17 @@
+package Dart;
+
+import javax.swing.*;
+
+public class Auswertung extends JFrame {
+
+    private JLabel labelDartAuswertung;
+    private JLabel labelGewonnen;
+    private JTextField textFieldGewonnen;
+    private JLabel labelPunktedurchschnitt;
+    private JTextField textFieldPunktedurchschnitt;
+    private JList listPunkteListe;
+    private JLabel labelPunkteliste;
+    private JButton buttonAufsteigendSortierenButton;
+    private JButton buttonAbsteigendSortierenButton;
+    private JButton buttonBeenden;
+}
---
+++ Projektarbeit 2025/ProgTechProjektarbeit2025/src/Dart/SpielerRunde.java
@@ -0,0 +1,52 @@
+package Dart;
+
+public class SpielerRunde {
+    //Attribute
+    private Spieler derzeitigerSpieler;
+    private int punkte;
+
+    //Methoden
+    public String getDerzeitigerSpieler() {
+        return derzeitigerSpieler;
+    }
+
+    public void setDerzeitigerSpieler(Spieler derzeitigerSpieler) {
+        this.derzeitigerSpieler = derzeitigerSpieler;
+    }
+
+    public int getPunkte() {
+        return punkte;
+    }
+
+    public void setPunkte(int punkte) {
+        this.punkte = punkte;
+    }
+
+    public boolean pruefeGewonnen() {
+        return;
+    }
+
+    public void wechselNaechsterspieler() {
+
+    }
+
+    public void verdoppelPunkte() {
+
+    }
+
+    public void verdreifachePunkte() {
+
+    }
+
+    public int statusGewinnPunkte(){
+        return;
+    }
+
+    public void verringereGewinnPunkte(){
+
+    }
+
+    public void direktZurAuswertung(){
+
+    }
+}
