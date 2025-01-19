f1bfc29435c6554d0b66804f5e6b1b059d17140d
Veldnastar <tim.engelhart@student.hnu.de>
2025-01-19 21:11:50 +0100

    Jans Teil
    -Klasse Spiel inklusive der form Datei
    -Methoden Grundriss für Klasse Auswertung

--- Projektarbeit 2025/ProgTechProjektarbeit2025/src/Dart/Auswertung.java
+++ Projektarbeit 2025/ProgTechProjektarbeit2025/src/Dart/Auswertung.java
@@ -15,4 +15,28 @@ public class Auswertung extends JFrame {
     private JButton buttonAufsteigendSortierenButton;
     private JButton buttonAbsteigendSortierenButton;
     private JButton buttonBeenden;
+
+    public double berechneDurchschnitt(){
+        int x = 0;
+        return x;
+    }
+
+    public void absteigendSortiert(){
+        //sortier algorithmus aufsteigend
+    }
+
+    public void aufsteigendSortiert(){
+        //Sortier algorithmus absteigend
+    }
+
+    public String gewonnenerSpieler(){
+        // return spieler mit nähester punktzahl zur 0 || 0;
+        String x = "ka";
+        return x;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
 }
--- Projektarbeit 2025/ProgTechProjektarbeit2025/src/Dart/Spiel.java
+++ Projektarbeit 2025/ProgTechProjektarbeit2025/src/Dart/Spiel.java
@@ -1,6 +1,52 @@
 package Dart;
 
-public class Spiel {
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
+public class Spiel extends JFrame {
+	private ArrayList<Spieler> String;
+	private ArrayList<Integer>punkteListe;
+	private JPanel spielPanel;
+	private JLabel SpielerLabel;
+	private JButton plusButton;
+	private JLabel dartSpielLabel;
+	private JList SpielerListeJList;
+	private JButton NeuesSpielButton;
+
+
+	public Spiel(){
+		setTitle("Dart");
+		setSize(300,300);
+		setContentPane(spielPanel);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+		SpielerListeJList.setBackground(Color.white);
+
+	}
+
+	public void addSpieler(){
+
+
+	}
+
+	public void addName(){
+
+
+	}
+
+	public void SpielStarten(){
+
+	}
+
+	public void SpielBeenden(){
+
+	}
+
+	public static void main(String[] args) {
+		new Spiel();
+	}
+
 
 
 
